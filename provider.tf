8d5be8595f8bd9cbd907412b2af4cf9ef10d41a7
Sanjay Singh <plus.sanjay.singh@gmail.com>
2021-12-30 01:46:30 +0530

    new brach commit

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance"{
-    ami = "ami-0ed9277fb7eb570c9"
+    ami = var.ami
     instance_type = "t2.micro"
     
 }
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "ami" {
+  default = "ami-0ed9277fb7eb570c9"
+}
